334ed21c0a34b5b1855ec791a2cd20ed30fcb869
Andrey Volkov <andrey.dreamwall@gmail.com>
2023-05-24 22:52:56 +0400

    add logs function

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from logging.config import dictConfig
 
 from dotenv import load_dotenv
 
-from settings import LogConfig
+from settings import LOGGING_CONFIG
 
 load_dotenv()
 
-dictConfig(LogConfig().dict())
-logger = logging.getLogger(LogConfig().name)
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+
+def logs():
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+    logger.critical("critical")
+
+
+if __name__ == "__main__":
+    logs()
